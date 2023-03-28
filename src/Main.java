e0327a025d0f5c092606ed48c16445d73a73687f
Harshad Rathod <rharshad26@gmail.com>
2023-03-28 13:22:49 +0530

    done plusMinus problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import round859.PlusMinus;
+
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PlusMinus pM = new PlusMinus();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.println(pM.checkPlusMinus(a, b, c));
+        }
     }
 }
---
+++ src/round859/PlusMinus.java
@@ -0,0 +1,7 @@
+package round859;
+
+public class PlusMinus {
+    public char checkPlusMinus(int a, int b, int c) {
+        return (a+b == c) ? '+' : '-';
+    }
+}
